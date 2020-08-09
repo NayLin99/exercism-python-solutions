80bc73eef0938fd457246217e4a39ed735e00b60
NayLin99 <naylin.ucsy.99@gmail.com>
2020-08-09 14:35:42 +0630

    add resistor-color-duo

---
+++ resistor-color-duo/resistor_color_duo.py
@@ -0,0 +1,6 @@
+COLOR = ["black", "brown", "red", "orange", "yellow",
+         "green", "blue", "violet", "grey", "white"]
+
+
+def value(colors):
+    return 10 * COLOR.index(colors[0]) + COLOR.index(colors[1])
